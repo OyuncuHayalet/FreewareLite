f996dc83424c87d9f31d7f811074efc4ccfcc7a2
OyuncuHayalet <82592303+OyuncuHayalet@users.noreply.github.com>
2022-07-26 12:43:45 +0300

    Add files via upload

---
+++ FreewareLite/FreewareLite/FileUtils.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace FreewareLite
+{
+	class FileUtils
+	{
+		public static string GetUserDirectoryPath()
+		{
+			string text = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName;
+			if (Environment.OSVersion.Version.Major >= 6)
+			{
+				text = Directory.GetParent(text).ToString();
+			}
+			return text;
+		}
+
+		public static string GetDrivePath()
+		{
+			string text = Path.GetPathRoot(Environment.SystemDirectory);
+			return text;
+		}
+
+		public static string GetDownloadTempPath()
+		{
+			string text = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.sonoyuncu\\bootstrap\\temp\\";
+			if (!Directory.Exists(text))
+			{
+				Directory.CreateDirectory(text);
+			}
+			return text;
+		}
+
+		public static string GetAppPath()
+		{
+			string text = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.sonoyuncu\\";
+			if (!Directory.Exists(text))
+			{
+				Directory.CreateDirectory(text);
+			}
+			return text;
+		}
+
+		public static string GetLauncherPath()
+		{
+			string text = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.sonoyuncu\\launcher.jar";
+			return text;
+		}
+
+		public static string GetExtractPath()
+		{
+			string text = FileUtils.GetAppPath() + "libraries\\extract";
+			if (!Directory.Exists(text))
+			{
+				Directory.CreateDirectory(text);
+			}
+			return text;
+		}
+
+		public static class Cryptography
+		{
+			public static string GetSHA1(string filePath)
+			{
+				string result = "";
+				try
+                {
+					using (FileStream fileStream = File.OpenRead(filePath))
+					{
+						using (SHA1Managed sha1Managed = new SHA1Managed())
+						{
+							result = BitConverter.ToString(sha1Managed.ComputeHash(fileStream)).Replace("-", string.Empty);
+						}
+					}
+				}
+				catch
+                {
+					return "F7AB9BFC1CC59E1BAA357CBDBB965388964CEDA0";
+                }
+				
+				return result;
+			}
+		}
+	}
+}
---
+++ FreewareLite/FreewareLite/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FreewareLite
+{
+    class Program
+    {
+        private static int nbPid = 0;
+        private static int javawPid = 0;
+        private static Process javaw = null;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "SonOyuncu ClientSide Xray Protection Disabler";
+
+            Process[] processList = Process.GetProcesses();
+
+            foreach (Process p in processList)
+            {
+                if (p.ProcessName.Equals("bootstrap"))
+                {
+                    nbPid = p.Id;
+                    break;
+                }
+            }
+
+            foreach (Process p in processList)
+            {
+                if (p.ProcessName.Equals("javaw") && p.MainWindowTitle.Equals("SonOyuncu Minecraft Launcher"))
+                {
+                    javawPid = p.Id;
+                    javaw = p;
+                    break;
+                }
+            }
+
+            if (javawPid == 0 || nbPid == 0)
+            {
+                MessageBox.Show("SonOyuncu Launcheri Açın", "MasterClient Launcher", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Environment.Exit(-1);
+            }
+
+            writer("lwjgl-2.9.4-nightly-20150209.jar lib dosyası editlenecek...", 10);
+
+            using (WebClient web = new WebClient())
+            {
+                web.DownloadFile("https://raw.githubusercontent.com/OyuncuHayalet/FreewareLite/main/FreewareLite/edit/lwjgl-2.9.4-nightly-20150209.jar", FileUtils.GetAppPath() + "libraries\\lwjgl-2.9.4-nightly-20150209.jar");
+                web.Dispose();
+            }
+
+            writer("Dosya değiştirildi... SonOyuncu bekleniyor...", 10);
+            
+            while (FileUtils.Cryptography.GetSHA1(FileUtils.GetAppPath() + "libraries\\lwjgl-2.9.4-nightly-20150209.jar") == "F7AB9BFC1CC59E1BAA357CBDBB965388964CEDA0")
+            {
+                Thread.Sleep(20);
+            }
+
+            using (WebClient web = new WebClient())
+            {
+                web.DownloadFile("https://raw.githubusercontent.com/OyuncuHayalet/FreewareLite/main/FreewareLite/edit/lwjgl-2.9.4-nightly-20150209.jar", FileUtils.GetAppPath() + "libraries\\lwjgl-2.9.4-nightly-20150209.jar");
+                web.Dispose();
+            }
+
+            writer("Lib dosyaası editlendi...", 10);
+            Thread.Sleep(2000);
+
+        }
+
+        public static void consolewriteline(string text, int delay)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(text[i]);
+                Thread.Sleep(delay);
+            }
+            Console.Write("\n");
+        }
+
+        public static void errorWriter(string text, int delay)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(" [ ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("#");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(" ] ");
+            Console.ForegroundColor = ConsoleColor.White;
+            consolewriteline(text, delay);
+        }
+
+        public static void writer(string text, int delay)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" [ ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("#");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" ] ");
+            Console.ForegroundColor = ConsoleColor.White;
+            consolewriteline(text, delay);
+        }
+    }
+}
